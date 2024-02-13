50f678f80bdd2610ecb3fa9194896db4c83626e9
SJibran Ahmed <Sheikhjibran889970@gmail.com>
2024-02-13 07:49:13 +0500

    new program

---
+++ program.js
@@ -0,0 +1,7 @@
+var owner_name = prompt('Enter your name');
+if (owner_name == 'Jibran') {
+    console.log("it's you Sheikh");
+}
+else {
+    console.log('Unknown person');
+}
---
+++ program.ts
@@ -0,0 +1,7 @@
+let owner_name : any = prompt('Enter your name')
+
+if (owner_name == 'Jibran'){
+    console.log("it's you Sheikh")
+} else{
+    console.log('Unknown person')
+}
